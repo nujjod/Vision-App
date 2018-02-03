29306ef4c6a65e6be1e0d9be43edf6c7c06e9265
Nujood Ahmed <nujjod@yahoo.com>
2018-02-03 15:04:03 +0300

    first commit

---
+++ Vision-app/View/RoundedButton.swift
@@ -0,0 +1,21 @@
+//
+//  RoundedButton.swift
+//  Vision-app
+//
+//  Created by Nujood on 2/3/18.
+//  Copyright © 2018 Nujood. All rights reserved.
+//
+
+import UIKit
+
+class RoundedButton: UIButton {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
---
+++ Vision-app/View/ٍRoundedView.swift
@@ -0,0 +1,21 @@
+//
+//  ٍRoundedView.swift
+//  Vision-app
+//
+//  Created by Nujood on 2/3/18.
+//  Copyright © 2018 Nujood. All rights reserved.
+//
+
+import UIKit
+
+class _RoundedView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
